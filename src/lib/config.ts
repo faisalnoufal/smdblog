c83e66178e43762783652b57ddd63dfaef9a07ef
faislnoufal <faisalnoufal@gmail.com>
2023-08-09 02:17:06 +0530

    updated description

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment'
 
 export const title = 'Crimefilez'
-export const description = 'SvelteKit blog for poets'
+export const description = 'Crime files'
 export const url = dev ? 'http://localhost:5173/' : 'https://crimefilez.com'
