d4136ce3a64f7b7a9a6ba86664dc3f5aa62a8fed
faislnoufal <faisalnoufal@gmail.com>
2023-08-12 04:28:50 +0530

    cards updated

--- src/components/Cards.svelte
+++ src/components/Cards.svelte
@@ -1,5 +1,5 @@
 <script>
-    	import { formatDate } from '$lib/utils'
+  	import { formatDate } from '$lib/utils'
 	import * as config from '$lib/config'
 
 	export let data
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,70 @@
 <script lang="ts">
-	import Cards from '../components/Cards.svelte';
+	// import Cards from '../components/Cards.svelte';
 	import Hero from '../components/Hero.svelte';
-	import { formatDate } from '$lib/utils'
-	import * as config from '$lib/config'
+	import { formatDate } from '$lib/utils';
+	import * as config from '$lib/config';
 
-	export let data
+	export let data;
+
+	function isPostNew(postDate) {
+		const twentyFourHoursAgo = new Date();
+		twentyFourHoursAgo.setDate(twentyFourHoursAgo.getDate() - 1);
+		return new Date(postDate) > twentyFourHoursAgo;
+	}
 </script>
 
 <svelte:head>
 	<title>{config.title}</title>
 </svelte:head>
 
-<Hero/>
+<Hero />
 
 <!-- <Cards/> -->
 
-  <!-- News Section -->
-  <section class="py-10">
-    <div class="container mx-auto px-4">
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        <!-- News Card 1 -->
-		{#each data.posts as post}
-        <div class="bg-white rounded-lg shadow-md p-6">
-			<img src={post.thumbnail} alt={post.title} class="w-full h-48 object-cover mb-4">
-			<h2 class="text-xl font-bold mb-2">{post.title}</h2>
-			<p class="text-gray-600">{post.description}</p>
-			<div class="flex justify-between items-center mt-2">
-			  <p class="text-gray-400 text-sm">{formatDate(post.date)}</p> <!-- Add the date section here -->
-			  <div>
-				<a href={post.slug} class="text-blue-500 hover:underline">Read More</a>
-				<button class="text-blue-500 hover:underline ml-4">Share</button>
-			  </div>
-			</div>
-		  </div>
-		{/each}
-
-      </div>
-    </div>
-  </section>
-
-
+<!-- News Section -->
+<section class="py-10">
+	<div class="container mx-auto px-8">
+		<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
+			<!-- News Card 1 -->
+			{#each data.posts as post}
+				<a href={post.slug}>
+					<div class="card bg-base-100 shadow-xl">
+						<figure><img src={post.thumbnail} alt={post.title} /></figure>
+						<div class="card-body">
+							<h2 class="card-title">
+								{post.title}
+								{#if isPostNew(post.date)}
+									<div class="badge badge-secondary">NEW</div>
+								{/if}
+							</h2>
+							<p>{post.description}</p>
+							<div class="flex items-center justify-between">
+								<p class="text-gray-400 text-sm mt-2">{formatDate(post.date)}</p>
+								<div class="space-x-2">
+									<!-- <a
+										href={`https://twitter.com/intent/tweet?url=https://hello.webadder.com/${post.slug}&text=${post.title}`}
+										class="text-blue-500 hover:underline"
+										target="_blank"
+									>
+										<i class="fab fa-twitter" /> Twitter
+									</a>
+									<a
+										href={`https://www.facebook.com/sharer/sharer.php?u=https://hello.webadder.com/${post.slug}`}
+										class="text-blue-500 hover:underline"
+										target="_blank"
+									>
+										<i class="fab fa-facebook" /> Facebook
+									</a> -->
+									<button class="material-symbols-outlined" on:click={copyPostURL}>Share</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</a>
+			{/each}
+		</div>
+	</div>
+</section>
 
 
 <!-- Posts -->
